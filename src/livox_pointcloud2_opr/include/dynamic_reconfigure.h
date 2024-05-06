316938b6d128310f2eddbd25fd90929295a37be2
QuintinUmi <qqj030212@gmail.com>
2024-05-06 23:26:15 +0800

    Encapsulating rqt reconfigure

---
+++ src/livox_pointcloud2_opr/include/dynamic_reconfigure.h
@@ -0,0 +1,69 @@
+#ifndef _DYNAMIC_RECONFIGURE_H_
+#define _DYNAMIC_RECONFIGURE_H_
+
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+
+#include <dynamic_reconfigure/server.h>
+#include <livox_pointcloud2_opr/PointcloudFilterConfig.h>
+
+
+namespace livox_pc2_opr
+{
+    struct RQTConfig
+    {
+        struct _FilterConfig
+        {
+            float x_min; 
+            float x_max;
+            float y_min;
+            float y_max;
+            float z_min;
+            float z_max;
+        }FilterConfig;
+
+        // struct LidarConfig
+        // {
+        //     float a;
+        //     float b;
+        // };
+        
+    };
+
+    class PointcloudFilterReconfigure 
+    {
+        public:
+            PointcloudFilterReconfigure();
+
+            RQTConfig::_FilterConfig get_configure();
+
+        private:
+            void FilterReconfigure_CallBack(livox_pointcloud2_opr::PointcloudFilterConfig &pcFilterConfig, uint32_t level);
+
+        private:
+            RQTConfig::_FilterConfig filterCfg;
+            dynamic_reconfigure::Server<livox_pointcloud2_opr::PointcloudFilterConfig> server;
+            dynamic_reconfigure::Server<livox_pointcloud2_opr::PointcloudFilterConfig>::CallbackType f;
+            
+    };
+
+    // class LidarReconfigure
+    // {
+    //     public:
+    //         LidarReconfigure();
+
+    //         void get_configure();
+
+        
+    // };
+
+}
+
+
+
+#endif
---
+++ src/livox_pointcloud2_opr/lib/dynamic_reconfigure.cpp
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+
+#include <dynamic_reconfigure/server.h>
+#include <livox_pointcloud2_opr/PointcloudFilterConfig.h>
+
+#include "dynamic_reconfigure.h"
+
+using namespace livox_pc2_opr;
+
+
+PointcloudFilterReconfigure::PointcloudFilterReconfigure() 
+{
+    this->f = boost::bind(&PointcloudFilterReconfigure::FilterReconfigure_CallBack, this, _1, _2);
+    this->server.setCallback(this->f);
+}
+
+void PointcloudFilterReconfigure::FilterReconfigure_CallBack(livox_pointcloud2_opr::PointcloudFilterConfig &pcFilterConfig, uint32_t level) 
+{
+    ROS_INFO("Filter Reconfigure: x_min=%f, x_max=%f, y_min=%f, y_max=%f, z_min=%f, z_max=%f",
+            pcFilterConfig.x_min, pcFilterConfig.x_max, pcFilterConfig.y_min, pcFilterConfig.y_max, pcFilterConfig.z_min, pcFilterConfig.z_max);
+    
+    this->filterCfg.x_min = pcFilterConfig.x_min;
+    this->filterCfg.x_max = pcFilterConfig.x_max;
+    this->filterCfg.y_min = pcFilterConfig.y_min;
+    this->filterCfg.y_max = pcFilterConfig.y_max;
+    this->filterCfg.z_min = pcFilterConfig.z_min;
+    this->filterCfg.z_max = pcFilterConfig.z_max;
+}
+
+RQTConfig::_FilterConfig PointcloudFilterReconfigure::get_configure()
+{
+    return this->filterCfg;
+}
