7e097ded4e5dab79227d8a50c4c439c12ed46fed
QuintinUmi <qqj030212@gmail.com>
2024-05-12 15:08:26 +0800

    Changes the return type. Added detection failure return flag for dynamic detection.

--- src/livox_pointcloud2_opr/src/caliboard_cloud_detection.cpp
+++ src/livox_pointcloud2_opr/src/caliboard_cloud_detection.cpp
@@ -32,52 +32,63 @@
 	{
 
 		livox_pc2_opr::PointCloud2Proc pc_process;
-		pc_process.loadPointCloudFile("src/livox_pointcloud2_opr/pcd/20240509061325.pcd");
+		std::vector<std::string> file_paths;
+		file_paths = pc_process.iterateFilesFromPath("src/livox_pointcloud2_opr/pcd");
+
+		for(const auto file_path:file_paths)
+		{
+			pc_process.loadPointCloudFile(file_path);
+			
+			// Detect caliboard corners
+			pcl::PointCloud<pcl::PointXYZI>::Ptr corners;
+			corners = pc_process.extractNearestRectangleCorners();
+
+			// Alternative
+			// pc_process.normalClusterExtraction();
+			// pc_process.extractNearestClusterCloud();
+			// pc_process.planeSegmentation();
+			// pc_process.planeProjection();
+			// pc_process.pcaTransform();
+			// pc_process.findRectangleCornersInPCAPlane();
+			// pc_process.transformCornersTo3D();
+			// pc_process.processedCloudUpdate(this->raw_cloud, this->computeNearestClusterIndices(this->raw_cloud, this->clusters));
+
+			// Visualized
+			pcl::visualization::PCLVisualizer viewer_final("Point Cloud Viewer");
+			viewer_final.setBackgroundColor(0, 0, 0);
+			
+
+			if(corners->size() != 0)
+			{
+				//Return corners
+				pcl::PointCloud<pcl::PointXYZI>::Ptr original_corners(pc_process.get3DRectCorners());
+
+				pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZI> red_color(original_corners, 255, 0, 0);
+				viewer_final.addPointCloud(original_corners, red_color, "corners");
+				viewer_final.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 10, "corners");
+
+				viewer_final.addLine((*original_corners)[0], (*original_corners)[1], "line1");
+				viewer_final.addLine((*original_corners)[1], (*original_corners)[2], "line2");
+				viewer_final.addLine((*original_corners)[2], (*original_corners)[3], "line3");
+				viewer_final.addLine((*original_corners)[3], (*original_corners)[0], "line4");
+			}
+			
+			pcl::PointCloud<pcl::PointXYZI>::Ptr rawCloud(pc_process.getRawPointcloud());
+			pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZI> rawCloud_color(rawCloud, 255, 255, 255);
+			viewer_final.addPointCloud(rawCloud, rawCloud_color, "raw_cloud");
+			viewer_final.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "raw_cloud");
+			
+			pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_plane(pc_process.getProcessedPointcloud());
+			pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZI> cloud_plane_color(cloud_plane, 0, 255, 0);
+			viewer_final.addPointCloud(cloud_plane, cloud_plane_color, "cloud_plane");
+			viewer_final.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5, "cloud_plane");
+
+
 		
-		// Detect caliboard corners
-		pc_process.extractNearestRectangleCorners();
-
-		// Alternative
-		// pc_process.normalClusterExtraction();
-		// pc_process.extractNearestClusterCloud();
-		// pc_process.planeSegmentation();
-		// pc_process.planeProjection();
-		// pc_process.pcaTransform();
-		// pc_process.findRectangleCornersInPCAPlane();
-		// pc_process.transformCornersTo3D();
-		// pc_process.processedCloudUpdate(this->raw_cloud, this->computeNearestClusterIndices(this->raw_cloud, this->clusters));
-
-		// Visualized
-		pcl::visualization::PCLVisualizer viewer_final("Point Cloud Viewer");
-		viewer_final.setBackgroundColor(0, 0, 0);
-		
-		//Return corners
-		pcl::PointCloud<pcl::PointXYZI>::Ptr original_corners(pc_process.get3DRectCorners());
-
-		pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZI> red_color(original_corners, 255, 0, 0);
-		viewer_final.addPointCloud(original_corners, red_color, "corners");
-		viewer_final.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 10, "corners");
-
-		viewer_final.addLine((*original_corners)[0], (*original_corners)[1], "line1");
-		viewer_final.addLine((*original_corners)[1], (*original_corners)[2], "line2");
-		viewer_final.addLine((*original_corners)[2], (*original_corners)[3], "line3");
-		viewer_final.addLine((*original_corners)[3], (*original_corners)[0], "line4");
-
-		pcl::PointCloud<pcl::PointXYZI>::Ptr rawCloud(pc_process.getRawPointcloud());
-		pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZI> rawCloud_color(rawCloud, 255, 255, 255);
-		viewer_final.addPointCloud(rawCloud, rawCloud_color, "raw_cloud");
-		viewer_final.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "raw_cloud");
-		
-		pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_plane(pc_process.getProcessedPointcloud());
-		pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZI> cloud_plane_color(cloud_plane, 0, 255, 0);
-		viewer_final.addPointCloud(cloud_plane, cloud_plane_color, "cloud_plane");
-		viewer_final.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5, "cloud_plane");
-
-
-    
-		while (!viewer_final.wasStopped()) {
-			viewer_final.spinOnce(100);
+			while (!viewer_final.wasStopped()) {
+				viewer_final.spinOnce(100);
+			}
 		}
-
+		
 		return 0;
 	}
